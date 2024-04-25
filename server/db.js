f6fa267c44c0f54ed21db0b24e086a55d08e1949
locust <locusttheeater@gmail.com>
2024-04-25 20:04:49 -0300

    drastic stuff
    very drastic. Mistakes were made. Many of them.

--- server/db.js
+++
@@ -1,5 +0,0 @@
-const Pool = require("pg").Pool;
-
-const Pool = new Pool({
-    //fill this with the focken postgresql server info. -> DBMAN TODO!
-})
--- server/index.js
+++ server/index.js
@@ -1,43 +1,20 @@
-//note 1: remember to save with encoding utf8 else shit will not work :>>>
+const express = require('express');
+const app = express();
 
-const express = require("express");
-const cors    = require("cors");
-const pool    = require("./db");
-const app     = express();
-
-//const port = 5000; //we'll follow strictly along the tutorial to avoid fuckups
-//middleware. Though i don't exactly understand the term just yet.
-
-app.use(cors());
-app.use(express.json());
-
-app.post("/newuser", async (req, res) => {
-    try {
-        const { placeholder } = req.body;
-        const newuser = await pool.query("DBMAN INSERT INFO", [placeholder]);
-        res.json(newuser.rows[0]);
-    }
-    catch (err)  { 
-        console.error(err.mesage);
-    }
+app.get('/', (req, res) => { 
+        res.send("home page");
     }
 )
 
-app.get("/user", async (req, res) => { 
-    try {
-        const someuser = await pool.query("DBMAN GET SPECIFIED USER");
-        res.json(someuser); //this should hold the user info after get query (?)
-    }
-    catch (err) { 
-        console.error(err.message);
-    }
+app.listen(5000, () => { 
+    console.log("server running on port 5000");
     }
 )
 
-app.listen(5000, () => {
-    console.log("running on port 5000"); //declared port up there literally for this :>
-}
-);
-
-
-
+//app.get
+//app.post
+//app.put
+//app.delete
+//app.all
+//app.use
+//app.listen
