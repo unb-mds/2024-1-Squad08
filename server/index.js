7f16d1e66b944c20e34b26efc733fe85bd01cb96
locust <locusttheeater@gmail.com>
2024-04-25 13:12:28 -0300

    very small formatting
    commiting this so i may merge the html next.

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 //note 1: remember to save with encoding utf8 else shit will not work :>>>
 
 const express = require("express");
-const cors = require("cors");
-const pool  = require("./db");
-const app = express();
+const cors    = require("cors");
+const pool    = require("./db");
+const app     = express();
 
 //const port = 5000; //we'll follow strictly along the tutorial to avoid fuckups
-
 //middleware. Though i don't exactly understand the term just yet.
+
 app.use(cors());
 app.use(express.json());
 
@@ -25,7 +25,7 @@ app.post("/newuser", async (req, res) => {
 
 app.get("/user", async (req, res) => { 
     try {
-        const someuser = await pool.query("DBMAN GET SPECIFIED USER")
+        const someuser = await pool.query("DBMAN GET SPECIFIED USER");
         res.json(someuser); //this should hold the user info after get query (?)
     }
     catch (err) { 
