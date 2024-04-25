dac6875b5d143a838ad81bdd61380304f1123277
locust <locusttheeater@gmail.com>
2024-04-25 20:23:21 -0300

    moved stuff, displayed stuff
    now the app
    now the app actually displays something.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+//const home = require('./crudehtml/index.html')
+
+app.use(express.static('./crudehtml'));
 
 app.get('/', (req, res) => { 
-        res.send("home page");
+        res.status(200).sendFile(path.resolve(__dirname, './crudehtml/index.html'));
     }
 )
+
+app.all('*', (req, res) => { 
+    res.status(404).send("<h1>NOT FOUND!</h1>");
+})
 
 app.listen(5000, () => { 
     console.log("server running on port 5000");
