dc546181bd3ebf9510d10879f7662177a6ca6195
Renan Camara <rcamaradeoliveirafarias@gmail.com>
2024-04-29 12:47:29 -0300

    Beta do webscrapper (finalizado)
    Com o uso do selenium, o site é acessado e seus dados são extraídos sem problemas. Para implementar: input vindo do API e retornar html para ser exibido pelo react

--- webscrapper/webscrapper.py
+++ webscrapper/webscrapper.py
@@ -1,20 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
 
 # Objetivo: Atraves do API, extrair cidade e estado preferível pelo usuário.
-# Segundo passo: Extrair o bairro de preferencia, se existir.
+# Retornar ao API os dados dos anuncios
+# Selenium é necessário para carregar o JS da página
+# Segundo passo: Extrair o título, preço, descrição, bairro e link de cada anuncio
 
-base_url ='https://www.webquarto.com.br/busca/quartos/'
+driver = webdriver.Chrome() # Utilizando o chrome
+url= "https://www.webquarto.com.br/busca/republicas/brasilia-df"
+driver.maximize_window()
+driver.get(url)
 
-# Formatação: cidade-estado
+time.sleep(5)
+content = driver.page_source.encode('utf-8').strip()
+soup = BeautifulSoup(content,"html.parser")
 
-local_user = input('Insira a cidade e estado: ') #Input temporario, não faz parte do proj final
-temp = local_user.split()
-local = temp[0] +'-'+ temp [1] #Formatação
+quartos = soup.findAll('div', attrs={'class', 'property-card'})
 
-# Pesquisar o local no site base
-resposta = requests.get(base_url + local)
-site = BeautifulSoup(resposta.text,'html.parser')
-print(site.prettify())
+for quarto in quartos:
+    titulo = quarto.find('h3', attrs={'class': 'text-overflow text-dark'})
+    preco= quarto.find('h5', attrs={'class': 'text-success m-t-0'})
+    descricao = quarto.find('p', attrs={'class': 'description font-13 text-muted m-b-0'})
+    bairro = quarto.find('p', attrs={'class':'text-muted text-overflow'})
+    link = quarto.find('a', attrs={'href':True})
 
-quarto = site.find('div', attrs={'class':'property-card'})
+    # Prints para testar o código, na prática, esse código retornará os códigos html de cada um para serem apresentados
+    print(titulo.text)
+    print(preco.text)
+    print(descricao.text)
+    print(bairro.text)
+    print(link['href'])
+    print('\n')
+
+# print(quarto.prettify())
+
