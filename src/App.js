76520c8602f219ac66d0617e5ac1caba3d57b8fd
Renurin <rcamaradeoliveirafarias@gmail.com>
2024-06-26 16:49:27 -0300

    not working, time to reset

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import cadastro from './components/pages/cadastro.js';
 import React from 'react';
 import { BrowserRouter as Router , Route, Routes} from "react-router-dom";
+import home from './components/pages/home.js'
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path = "/" element={<cadastro/>} />
+          <Route path = "/cadastro" element={root.render(<cadastro/>)} />
+          <Route path = "/" element={<home/>} />
         </Routes>
       </Router>
     </div>
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import cadastro from './components/pages/cadastro.js'
 
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
