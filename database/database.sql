2fd5bbffd997400532e6d4229f154c24fbb611e8
locust <locusttheeater@gmail.com>
2024-06-29 12:44:49 -0300

    New database creation script
    V1 mostly done, might need more changes, such as standardizing the names, but, still, mostly done.

--- database/database.sql
+++ database/database.sql
@@ -1,40 +1,56 @@
--- Tabela Usuário
-CREATE TABLE Usuario 
-( 
-  id_usuario INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,  
-  email VARCHAR(255) NOT NULL,  
-  contato VARCHAR(15) NOT NULL,  
-  senha VARCHAR(255) NOT NULL,  
-  genero VARCHAR(10) NOT NULL,
-  data_nascimento DATE,  
-  cpf VARCHAR(14) NOT NULL,  
-  nome VARCHAR(255) NOT NULL,  
-  UNIQUE (email), 
-  UNIQUE (contato),
-  UNIQUE (cpf)
-); 
+/*
+written by BadCodeL0cvst, whose database is a terrible mess which should never be sent to production.
 
--- Tabela Quarto
-CREATE TABLE Quarto 
-( 
-  id_quarto INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,  
-  preco INT,  
-  descricao VARCHAR(255),  
-  local VARCHAR(255),  
-  genero VARCHAR(10),  
-  id_anunciante INT,
-  FOREIGN KEY (id_anunciante) REFERENCES Usuario (id_usuario) 
-); 
+note1: on REPUBLICA table
+	see that emaildono? it refers to the owner of the real state up for rent. 
+    Keep it in mind when creating the api's, if you try to register a tenant in this table it 
+	will MFU the entire database logic. Beware :)
 
--- Tabela Reserva
-CREATE TABLE Reserva 
-( 
-  id_reserva INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  data_checkin DATE,  
-  data_checkout DATE,  
-  id_quarto INT,  
-  id_reservante INT,  
-  data_reserva DATE,
-  FOREIGN KEY (id_quarto) REFERENCES Quarto (id_quarto),
-  FOREIGN KEY (id_reservante) REFERENCES Usuario (id_usuario)
-);
+
+*/
+
+CREATE TABLE USUARIO (
+    emailusuario VARCHAR(256),
+    nome VARCHAR(256) NOT NULL,
+    senha VARCHAR(256) NOT NULL,
+    idImUsuario BIGINT,
+    CONSTRAINT USUARIO_PK PRIMARY KEY (emailusuario),
+    CONSTRAINT USUARIO_IMAGEM_FK FOREIGN KEY (idImUsuario) REFERENCES IMAGEMUSUARIO(idImUsuario) ON DELETE RESTRICT
+) ENGINE = InnoDB;
+
+CREATE TABLE TELEFONEUSUARIO(
+	telefone VARCHAR(32),
+    emailusuario VARCHAR(256),
+    CONSTRAINT TELEFONEUSUARIO_UK UNIQUE (telefone),
+    CONSTRAINT TELEFONEUSUARIO_USUARIO_FK FOREIGN KEY (emailusuario) REFERENCES USUARIO(emailusuario) ON DELETE CASCADE ON UPDATE CASCADE    
+)ENGINE = InnoDB;
+
+CREATE TABLE REPUBLICA (
+    idRepublica BIGINT PRIMARY KEY,
+    nome VARCHAR(256),
+    cep VARCHAR(256),
+    complemento VARCHAR(256),
+    numero INT,
+    emaildono VARCHAR(256),
+    CONSTRAINT REPUBLICA_USUARIO_FK FOREIGN KEY (emaildono) REFERENCES USUARIO (emailusuario) ON DELETE RESTRICT
+)ENGINE = InnoDB;
+
+CREATE TABLE ALUGUEL (
+    idAluguel BIGINT PRIMARY KEY,
+    FK_USUARIO_emailusuario VARCHAR(256),
+    FK_REPUBLICA_idRepublica BIGINT,
+    CONSTRAINT FK_ALUGUEL_2 FOREIGN KEY (FK_USUARIO_emailusuario) REFERENCES USUARIO (emailusuario) ON DELETE RESTRICT,
+    CONSTRAINT FK_ALUGUEL_3 FOREIGN KEY (FK_REPUBLICA_idRepublica) REFERENCES REPUBLICA (idRepublica) ON DELETE RESTRICT
+)ENGINE = InnoDB;
+
+CREATE TABLE IMAGEMREPUBLICA (
+    idImRepublica BIGINT PRIMARY KEY,
+    caminhoImRepublica VARCHAR(256),
+    FK_REPUBLICA_idRepublica BIGINT,
+    CONSTRAINT FK_IMAGEMREPUBLICA_2 FOREIGN KEY (FK_REPUBLICA_idRepublica) REFERENCES REPUBLICA (idRepublica) ON DELETE RESTRICT
+)ENGINE = InnoDB;
+
+CREATE TABLE IMAGEMUSUARIO (
+    idImUsuario BIGINT PRIMARY KEY,
+    caminhoImUsuario VARCHAR(256)
+)ENGINE = InnoDB;
