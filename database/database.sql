4d950f66065ba4aa4c26df9f0d4439fbfb504f31
locust <locusttheeater@gmail.com>
2024-06-29 15:04:33 -0300

    Standardization of constraint names on database setup
    FK constraints now are named TABLENAME_FOREIGNTABLE_FK_NUMIFNEEDED
    UK and PK constraints follow the same name standard.
    Might be easier to alter them if needed...
    Also, attribute names are now named after their use inside the table and/or named the same as their origin table instead of having FK_TABLENAME_attributename standard. Easier to reference when inserting values and such.

--- database/database.sql
+++ database/database.sql
@@ -6,6 +6,11 @@ note1: on REPUBLICA table
     Keep it in mind when creating the api's, if you try to register a tenant in this table it 
 	will MFU the entire database logic. Beware :)
 
+note2: About user deletion
+	If the user gets deleted, so will his personal number used to setup the account. Beware that!
+	The DataBase should restrict the user deletion if he got a rent setup or if he got a republic 
+    set up to be rented out. 
+
 
 */
 
@@ -14,6 +19,7 @@ CREATE TABLE USUARIO (
     nome VARCHAR(256) NOT NULL,
     senha VARCHAR(256) NOT NULL,
     idImUsuario BIGINT,
+    CONSTRAINT USUARIO_UK UNIQUE (nome),
     CONSTRAINT USUARIO_PK PRIMARY KEY (emailusuario),
     CONSTRAINT USUARIO_IMAGEM_FK FOREIGN KEY (idImUsuario) REFERENCES IMAGEMUSUARIO(idImUsuario) ON DELETE RESTRICT
 ) ENGINE = InnoDB;
@@ -22,7 +28,7 @@ CREATE TABLE TELEFONEUSUARIO(
 	telefone VARCHAR(32),
     emailusuario VARCHAR(256),
     CONSTRAINT TELEFONEUSUARIO_UK UNIQUE (telefone),
-    CONSTRAINT TELEFONEUSUARIO_USUARIO_FK FOREIGN KEY (emailusuario) REFERENCES USUARIO(emailusuario) ON DELETE CASCADE ON UPDATE CASCADE    
+    CONSTRAINT TELEFONEUSUARIO_USUARIO_FK FOREIGN KEY (emailusuario) REFERENCES USUARIO(emailusuario) ON DELETE CASCADE ON UPDATE CASCADE -- this means if the user gets deleted, his personal info also goes caput.   
 )ENGINE = InnoDB;
 
 CREATE TABLE REPUBLICA (
@@ -32,25 +38,28 @@ CREATE TABLE REPUBLICA (
     complemento VARCHAR(256),
     numero INT,
     emaildono VARCHAR(256),
-    CONSTRAINT REPUBLICA_USUARIO_FK FOREIGN KEY (emaildono) REFERENCES USUARIO (emailusuario) ON DELETE RESTRICT
+    CONSTRAINT REPUBLICA_USUARIO_FK FOREIGN KEY (emaildono) REFERENCES USUARIO (emailusuario) ON DELETE CASCADE ON UPDATE CASCADE 
 )ENGINE = InnoDB;
 
 CREATE TABLE ALUGUEL (
-    idAluguel BIGINT PRIMARY KEY,
-    FK_USUARIO_emailusuario VARCHAR(256),
-    FK_REPUBLICA_idRepublica BIGINT,
-    CONSTRAINT FK_ALUGUEL_2 FOREIGN KEY (FK_USUARIO_emailusuario) REFERENCES USUARIO (emailusuario) ON DELETE RESTRICT,
-    CONSTRAINT FK_ALUGUEL_3 FOREIGN KEY (FK_REPUBLICA_idRepublica) REFERENCES REPUBLICA (idRepublica) ON DELETE RESTRICT
-)ENGINE = InnoDB;
+    idAluguel BIGINT AUTO_INCREMENT,
+    emailusuario VARCHAR(256),
+    idRepublica BIGINT,
+    CONSTRAINT ALUGUEL_PK PRIMARY KEY (idAluguel),
+    CONSTRAINT ALUGUEL_USUARIO_FK FOREIGN KEY (emailusuario) REFERENCES USUARIO(emailusuario) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT ALUGUEL_REPUBLICA_FK FOREIGN KEY (idRepublica) REFERENCES REPUBLICA(idRepublica) ON DELETE RESTRICT
+)ENGINE = InnoDB AUTO_INCREMENT = 1;
 
 CREATE TABLE IMAGEMREPUBLICA (
     idImRepublica BIGINT PRIMARY KEY,
     caminhoImRepublica VARCHAR(256),
-    FK_REPUBLICA_idRepublica BIGINT,
-    CONSTRAINT FK_IMAGEMREPUBLICA_2 FOREIGN KEY (FK_REPUBLICA_idRepublica) REFERENCES REPUBLICA (idRepublica) ON DELETE RESTRICT
+    idRepublica BIGINT,
+    CONSTRAINT IMAGEMREPUBLICA_REPUBLICA_FK FOREIGN KEY (idRepublica) REFERENCES REPUBLICA(idRepublica) ON DELETE CASCADE ON UPDATE CASCADE
 )ENGINE = InnoDB;
 
 CREATE TABLE IMAGEMUSUARIO (
-    idImUsuario BIGINT PRIMARY KEY,
-    caminhoImUsuario VARCHAR(256)
-)ENGINE = InnoDB;
+    idImUsuario BIGINT AUTO_INCREMENT,
+    caminhoImUsuario VARCHAR(256),
+    CONSTRAINT IMAGEMUSUARIO_PK PRIMARY KEY (idImUsuario),
+    CONSTRAINT IMAGEMUSUARIO_UK UNIQUE (caminhoImUsuario)
+)ENGINE = InnoDB AUTO_INCREMENT = 1;
