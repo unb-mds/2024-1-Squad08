63aecc8f0a1e74846b8ed99564255cdc3d22d8a6
locust <locusttheeater@gmail.com>
2024-06-30 10:56:32 -0300

    nuking script done; adjustments on creation script
    finishing up this mess.
    Next should be the pop script, the most boring ass thing to write.

--- database/databasecreationscript.sql
+++ database/databasecreationscript.sql
@@ -1,6 +1,8 @@
 /*
 written by BadCodeL0cvst, whose database is a terrible mess which should never be sent to production.
 
+This database should hold basic info on users and available republics for renting.
+
 note1: on REPUBLICA table
 	see that emaildono? it refers to the owner of the real state up for rent. 
     Keep it in mind when creating the api's, if you try to register a tenant in this table it 
@@ -10,16 +12,24 @@ note2: About user deletion
 	If the user gets deleted, so will his personal number used to setup the account. Beware that!
 	The DataBase should restrict the user deletion if he got a rent setup or if he got a republic 
     set up to be rented out. 
-
-
 */
+CREATE DATABASE IF NOT EXISTS unirepdb_basic;
+
+use unirepdb_basic;
+
+CREATE TABLE IMAGEMUSUARIO (
+    idImUsuario BIGINT NOT NULL AUTO_INCREMENT,
+    caminhoImUsuario VARCHAR(256) NOT NULL,
+    CONSTRAINT IMAGEMUSUARIO_PK PRIMARY KEY (idImUsuario),
+    CONSTRAINT IMAGEMUSUARIO_UK UNIQUE (caminhoImUsuario)
+)ENGINE = InnoDB AUTO_INCREMENT = 1;
 
 CREATE TABLE USUARIO (
-    emailusuario VARCHAR(256),
-    nome VARCHAR(256) NOT NULL,
+    emailusuario VARCHAR(256) NOT NULL,
+    nomeusuario VARCHAR(256) NOT NULL,
     senha VARCHAR(256) NOT NULL,
     idImUsuario BIGINT,
-    CONSTRAINT USUARIO_UK UNIQUE (nome),
+    CONSTRAINT USUARIO_UK_1 UNIQUE (nomeusuario),
     CONSTRAINT USUARIO_PK PRIMARY KEY (emailusuario),
     CONSTRAINT USUARIO_IMAGEM_FK FOREIGN KEY (idImUsuario) REFERENCES IMAGEMUSUARIO(idImUsuario) ON DELETE RESTRICT
 ) ENGINE = InnoDB;
@@ -32,14 +42,15 @@ CREATE TABLE TELEFONEUSUARIO(
 )ENGINE = InnoDB;
 
 CREATE TABLE REPUBLICA (
-    idRepublica BIGINT PRIMARY KEY,
+    idRepublica BIGINT AUTO_INCREMENT,
     nome VARCHAR(256),
     cep VARCHAR(256),
     complemento VARCHAR(256),
     numero INT,
     emaildono VARCHAR(256),
-    CONSTRAINT REPUBLICA_USUARIO_FK FOREIGN KEY (emaildono) REFERENCES USUARIO (emailusuario) ON DELETE CASCADE ON UPDATE CASCADE 
-)ENGINE = InnoDB;
+    CONSTRAINT REPUBLICA_PK PRIMARY KEY (idRepublica),
+    CONSTRAINT REPUBLICA_USUARIO_FK FOREIGN KEY (emaildono) REFERENCES USUARIO(emailusuario) ON DELETE CASCADE ON UPDATE CASCADE 
+)ENGINE = InnoDB AUTO_INCREMENT = 1;
 
 CREATE TABLE ALUGUEL (
     idAluguel BIGINT AUTO_INCREMENT,
@@ -51,15 +62,9 @@ CREATE TABLE ALUGUEL (
 )ENGINE = InnoDB AUTO_INCREMENT = 1;
 
 CREATE TABLE IMAGEMREPUBLICA (
-    idImRepublica BIGINT PRIMARY KEY,
+    idImRepublica BIGINT AUTO_INCREMENT,
     caminhoImRepublica VARCHAR(256),
     idRepublica BIGINT,
+    CONSTRAINT IMAGEMREPUBLICA_PK PRIMARY KEY (idImRepublica),
     CONSTRAINT IMAGEMREPUBLICA_REPUBLICA_FK FOREIGN KEY (idRepublica) REFERENCES REPUBLICA(idRepublica) ON DELETE CASCADE ON UPDATE CASCADE
-)ENGINE = InnoDB;
-
-CREATE TABLE IMAGEMUSUARIO (
-    idImUsuario BIGINT AUTO_INCREMENT,
-    caminhoImUsuario VARCHAR(256),
-    CONSTRAINT IMAGEMUSUARIO_PK PRIMARY KEY (idImUsuario),
-    CONSTRAINT IMAGEMUSUARIO_UK UNIQUE (caminhoImUsuario)
 )ENGINE = InnoDB AUTO_INCREMENT = 1;
---
+++ database/databasepopulationscript.sql
@@ -0,0 +1,7 @@
+/*
+Written by BCL0c, whose inserts will most likely fail.
+
+This is the population script for the database. It inserts a lot of stuff so we can 
+test it out and work on subsequent changes. 
+
+*/
