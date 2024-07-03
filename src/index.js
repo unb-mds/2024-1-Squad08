4f77319a923abe014859c23bc43bfdc7a4bc6ec9
Renurin <rcamaradeoliveirafarias@gmail.com>
2024-07-03 16:53:43 -0300

    FINALLY, somethin happen, still has to fix app.js

--- src/index.js
+++ src/index.js
@@ -3,19 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-=======
 import cadastro from './components/pages/cadastro.js'
+import { renderToStaticMarkup } from 'react-dom/server';
 
->>>>>>> 76520c8602f219ac66d0617e5ac1caba3d57b8fd
+const domNode = document.getElementById('root')
+const root = ReactDOM.createRoot(domNode);
+root.render(<App></App>);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
