d47b18be35c1eadee513445e43996c9742f22ea1
locust <locusttheeater@gmail.com>
2024-08-03 14:30:15 -0300

    Made search into a component
    Will help with reading this mess and stuff.

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -25,6 +25,30 @@ class PersonLogo extends React.Component {
   }
 }
 
+class search extends React.Component { 
+  render() { 
+    return (
+      <div className="imagem">
+              <img src={homeImage} alt="" style={{ marginTop: 30 }} />
+              <div className="search">
+                <input
+                  type="text"
+                  className="search__input"
+                  placeholder="Digite o local de busca:"
+                />
+                <button className="search__button">
+                  <svg className="search__icon" aria-hidden="true" viewBox="0 0 24 24">
+                    <g>
+                      <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z" />
+                    </g>
+                  </svg>
+                </button>
+              </div>
+            </div>
+    )
+  }
+}
+
 class home extends React.Component {
   constructor(props){ 
     super(props)
@@ -32,7 +56,6 @@ class home extends React.Component {
   render() {
     return (
       <div>
-        <>
           <meta charSet="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <title>UniRep</title>
@@ -54,23 +77,7 @@ class home extends React.Component {
               </a>
               <PersonLogo />
             </div>
-            <div className="imagem">
-              <img src={homeImage} alt="" style={{ marginTop: 30 }} />
-              <div className="search">
-                <input
-                  type="text"
-                  className="search__input"
-                  placeholder="Digite o local de busca:"
-                />
-                <button className="search__button">
-                  <svg className="search__icon" aria-hidden="true" viewBox="0 0 24 24">
-                    <g>
-                      <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z" />
-                    </g>
-                  </svg>
-                </button>
-              </div>
-            </div>
+            <Search/>
             <div className="choice"></div>
           </div>
           <div className="secondPart" id="Populares">
@@ -319,7 +326,6 @@ class home extends React.Component {
               </div>
             </div>
           </div>
-        </>
       </div>
     )
   }
